edf71082423f9102c5ed725d4f43f31d9ccb45e8
sdondon <sdondon@hotmail.com>
2019-02-24 01:09:38 +0900

    홈 - 하단 인풋 섹션 추가

---
+++ RealWeather/HomeBottomInputViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeBottomInputViewController.swift
+//  RealWeather
+//
+//  Created by sdondon on 24/02/2019.
+//  Copyright © 2019 yapp. All rights reserved.
+//
+
+import UIKit
+
+class HomeBottomInputViewController: UIViewController {
+    @IBOutlet fileprivate weak var profileView: UIView!
+    @IBOutlet fileprivate weak var commentTextField: UITextField!
+    
+    @IBAction func onRegisterCommentBtnTapped(_ sender: UIButton) {
+        
+    }
+}
