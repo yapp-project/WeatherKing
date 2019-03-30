90b602b928acd1b9874d486407ccf7b2367240fe
uhh77 <32029784+uhh77@users.noreply.github.com>
2019-03-31 07:50:36 +0900

    시작 페이지, 닉네임 확인 (#3)

---
+++ RealWeather/Start/NickNameViewController.swift
@@ -0,0 +1,43 @@
+//
+//  NickNameViewController.swift
+//  RealWeather
+//
+//  Created by yoo on 2019. 3. 31..
+//  Copyright © 2019년 yapp. All rights reserved.
+//
+
+import UIKit
+
+class NickNameViewController: UIViewController {
+    @IBOutlet weak var nickField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneBtn = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.AcrionBar))
+        
+        toolbar.items = [doneBtn]
+        nickField.inputView = toolbar
+    }
+    
+    @objc func AcrionBar() {
+        view.endEditing(true)
+    }
+    @IBAction func okBtn(_ sender: Any) {
+        let stroyboard:UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let nextView = stroyboard.instantiateViewController(withIdentifier: "main")
+        
+        self.present(nextView,animated: true,completion: nil)
+    }
+}
+
+extension NickNameViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = nickField.text else { return true }
+        let newLength = text.characters.count + string.characters.count - range.length
+        return newLength <= 8
+    }
+}
